b954de62a6f1fe431e66b8cc3a616731352760f9
Sherief Farid <sherief@farid.cloud>
2019-02-10 20:41:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,8 +24,9 @@ def end_game(card_total)
 end
 
 def initial_round
-
-  card_total = deal_card + deal_card
+  card1 = deal_card
+  card2 = deal_card
+  card_total = card1 + card2
   puts display_card_total(card_total)
 end
 
