4104da7f1a491b0c1ec8e89e4c956a8fc56c34cb
Sherief Farid <sherief@farid.cloud>
2019-02-10 20:37:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -12,11 +12,11 @@ end
 
 def prompt_user
  puts "Type 'h' to hit or 's' to stay"
- response = gets.chomp
+
 end
 
 def get_user_input
-  # code #get_user_input here
+ response = gets.chomp
 end
 
 def end_game
